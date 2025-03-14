5c1919eb43ceb580419feb3f69a0dbf64a8706be
emmanuel-toast <emmanuel@toast.co.nz>
2025-03-14 15:50:01 +1300

    updated the preview refresh functionality again so that the preview needs a bit more convincing before it decides to refresh

--- client/source/scripts/index.js
+++ client/source/scripts/index.js
@@ -7,6 +7,9 @@ let input = null;
 let container = null;
 let refreshTimeout = null;
 let isRefreshing = false;
+let mutationCount = [];
+let mutationTimeout = null;
+let currentURL = '';
 let iframe = document.createElement('iframe');
 
 // Selectors for the input and preview panel elements
@@ -120,31 +123,62 @@ function addPreview() {
 }
 
 function removePreview() {
+  // Clear the current URL
+  currentURL = '';
+
+  // Remove the preview panel if it exists
   if (document.body.contains(previewPanel)) {
     previewPanel.remove();
   }
 }
 
+const VisibleMutationObserver = new IntersectionObserver((entries) => {
+  // Loop through each entry and check if it is visible
+  entries.forEach((entry) => {
+    if (entry.isIntersecting) {
+      mutationCount.push(entry);
+    }
+
+    VisibleMutationObserver.unobserve(entry.target);
+  });
+});
+
+
 const Observer = new MutationObserver((mutations) => {
+  // Clear the previous timeout
+  clearTimeout(mutationTimeout);
+
+  // Set a timeout to reset the mutation count after 1 second
+  mutationTimeout = setTimeout(() => {
+    // Clear the previous visible mutations
+    mutationCount.length = 0;
+  }, 50);
+
   // Check if the input exists
   input = document.querySelector(selectors.input);
   // Check if the container exists
   container = document.querySelector(selectors.container);
 
-  // Array to store added nodes
-  const addedNodes = [];
-
   // Loop through each mutation and check for elements that were added (excluding the preview panel)
   mutations.forEach(mutation => {
-    mutation.addedNodes.forEach(node => {
-      if (!previewPanel.contains(node) && node.nodeType === 1) {
-        addedNodes.push(node);
-      }
+    mutation.addedNodes.forEach((node) => {
+      if (node.nodeType !== 1) return;
+      if (previewPanel.contains(node)) return;
+
+      VisibleMutationObserver.observe(node);
     });
   });
 
-  // Check if any nodes were added (excluding the preview panel)
-  if (addedNodes.length) {
+  // Check if there are 3 or more mutations within 50ms
+  if (mutationCount.length >= 3 || window.location.href !== currentURL) {
+
+    // Update the current URL
+    currentURL = window.location.href;
+
+    // Clear the previous visible mutations
+    mutationCount.length = 0;
+
+    console.log('updating preview');
     // Show or hide the preview based on the presence of the input
     (input && container) ? addPreview() : removePreview();
   }
@@ -157,127 +191,3 @@ Observer.observe(document.body, {
 });
 
 makePreviewResizeable();
-
-
-// const defaultSrc = 'about:blank?CMSPreview=true';
-// const previewPanel = document.createElement('div');
-// const iframe = document.createElement('iframe');
-
-// let input = null;
-// let container = null;
-// let refreshTimeout = null;
-// let isRefreshing = false;
-
-// // Selectors for the input and preview panel elements
-// const selectors = {
-//   container: '.cms-container',
-//   input: '[name="OpenCMSPreview"]',
-// }
-
-// // Set up some classes and put the iframe into the preview panel
-// iframe.src = defaultSrc;
-// iframe.className = 'open-cms-preview__iframe';
-// previewPanel.classList.add('open-cms-preview');
-// previewPanel.appendChild(iframe);
-
-// // Function to update the preview src
-// function updatePreview() {
-//   if (isRefreshing) return;
-
-//   clearTimeout(refreshTimeout);
-
-//   isRefreshing = true;
-
-//   refreshTimeout = setTimeout(() => {
-//     iframe.src = defaultSrc;
-
-//     clearTimeout(refreshTimeout);
-
-//     refreshTimeout = setTimeout(() => {
-//       isRefreshing = false;
-
-//       if (!input) return;
-
-//       iframe.src = input.value;
-//     }, 500);
-//   }, 500);
-// }
-
-// function makePreviewResizeable() {
-//   // We will toggle this to toggle the dragging state
-//   let isDragging = false;
-
-//   // Create a new button element to drag the preview width
-//   const thumb = document.createElement('button');
-
-//   // Add a class
-//   thumb.classList.add('open-cms-preview__thumb');
-
-//   // Add the thumb to the preview
-//   previewPanel.appendChild(thumb);
-
-//   // Add a mouse down event listener
-//   thumb.addEventListener('mousedown', () => {
-//     // Set dragging to true
-//     isDragging = true;
-//     previewPanel.classList.add('dragging');
-//   });
-
-//   window.addEventListener('mouseup', () => {
-//     // Set dragging to false
-//     isDragging = false;
-//     previewPanel.classList.remove('dragging');
-//   });
-
-//   window.addEventListener('mousemove', (e) => {
-//     // Set the width of the preview
-//     window.requestAnimationFrame(() => {
-//       // If we are not dragging, return
-//       if (!isDragging) return;
-
-//       const mouseX = (e.clientX - 10);
-//       const width = window.innerWidth - mouseX;
-
-//       document.body.style.setProperty('--preview-width', `${width}px`);
-//     });
-//   });
-// }
-
-// function addPreview() {
-//   if (!document.body.contains(previewPanel)) {
-//     container.appendChild(previewPanel);
-//   }
-
-//   // Update the preview
-//   updatePreview();
-// }
-
-// function removePreview() {
-//   if (document.body.contains(previewPanel)) {
-//     previewPanel.remove();
-//   }
-// }
-
-// const Observer = new MutationObserver((mutations) => {
-//   // Check if the input exists
-//   input = document.querySelector(selectors.input);
-//   // Check if the container exists
-//   container = document.querySelector(selectors.container);
-
-//   // If any of the mutations involve added or removed element nodes
-//   if (mutations.some((mutation) =>
-//     Array.from(mutation.addedNodes).some(node => node.nodeType === 1) ||
-//     Array.from(mutation.removedNodes).some(node => node.nodeType === 1)
-//   )) {
-//     // Show or hide the preview based on the presence of the input
-//     (input && container) ? addPreview() : removePreview();
-//   }
-// });
-
-// // Watch the body for changes
-// Observer.observe(document.body, {
-//   childList: true,
-//   subtree: true,
-// });
-
-// makePreviewResizeable();
